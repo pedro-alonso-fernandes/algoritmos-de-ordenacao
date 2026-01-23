fbbeeefe4fb5bfa49e29d11e83ac9419c95aa28f
Talles Bazani <tallesbazani@live.com>
2026-01-23 20:58:40 -0300

    Adiciona temporizador no main

--- main.c
+++ main.c
@@ -1,20 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "geranum.h"
 #include "algoritmos.h"
 
 int main(int argc, char** argv){
 
+	clock_t inicio, fim;
+	double tempo;
+
 	printf("\n");
 	printf("--------------------------------------------------------\n");
 	
 
-	gerarNumeros(5, ALEATORIO);
-	int* registro = ordenarNumeros(HEAPSORT);
+	gerarNumeros(500000, ALEATORIO);
 
+	inicio = clock();
+	int* registro = ordenarNumeros(HEAPSORT);
+	fim = clock();
+
+	tempo = (double)(fim - inicio) / CLOCKS_PER_SEC;
 
 	printf("Qtd Comparações: %d\n", registro[0]);
 	printf("Qtd trocas: %d\n", registro[1]);
+	printf("Tempo de execução: %.6f segundos\n", tempo);
 
 	printf("--------------------------------------------------------\n");
 	printf("\n");
